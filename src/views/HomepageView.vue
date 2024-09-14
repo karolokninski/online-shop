a7e7976295016110f180f4af0f54ce98f3512777
jacek <69kotkiwpiwnicy69@gmail.com>
2024-09-14 20:48:25 +0200

    usuniecie przycisku

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -9,10 +9,7 @@
           </a>
         </div>
         <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-          </button>
+          
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
           <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>

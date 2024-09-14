443c710e1d37f62b9a0039efe211f3b4280cf5d2
jacek <69kotkiwpiwnicy69@gmail.com>
2024-09-14 20:34:58 +0200

    modified:   package-lock.json 	new file:   src/assets/logo.png 	modified:   src/views/HomepageView.vue 	modified:   src/views/RegistrationView.vue 	modified:   src/views/SignInView.vue

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -5,7 +5,7 @@
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <img class="h-12 w-auto" src="../assets/logo.png" alt="" />
           </a>
         </div>
         <div class="flex lg:hidden">

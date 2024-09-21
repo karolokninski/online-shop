6d8078e108d9a46f4e558f141cbff4ec9183c973
kepucino <okninskikarol2@gmail.com>
2024-09-21 16:00:09 +0200

    Add products pinia store Add product to cart feature

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -26,7 +26,7 @@
                     <div class="mt-8">
                       <div class="flow-root">
                         <ul role="list" class="-my-6 divide-y divide-gray-200">
-                          <li v-for="product in products" :key="product.id" class="flex py-6">
+                          <li v-for="product in shoppingCartStore.products" :key="product.id" class="flex py-6">
                             <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                               <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center" />
                             </div>
@@ -37,12 +37,12 @@
                                   <h3>
                                     <a :href="product.href">{{ product.name }}</a>
                                   </h3>
-                                  <p class="ml-4">{{ product.price }}</p>
+                                  <p class="ml-4">{{ product.price }} PLN</p>
                                 </div>
                                 <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
                               </div>
                               <div class="flex flex-1 items-end justify-between text-sm">
-                                <p class="text-gray-500">Qty {{ product.quantity }}</p>
+                                <p class="text-gray-500">{{ product.quantity }} szt.</p>
 
                                 <div class="flex">
                                   <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
@@ -90,29 +90,4 @@
   import { useShoppingCartStore } from '@/stores/shoppingCart'
   
   const shoppingCartStore = useShoppingCartStore()
-  
-  const products = [
-    {
-      id: 1,
-      name: 'Throwback Hip Bag',
-      href: '#',
-      color: 'Salmon',
-      price: '$90.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-      imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-    },
-    {
-      id: 2,
-      name: 'Medium Stuff Satchel',
-      href: '#',
-      color: 'Blue',
-      price: '$32.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-      imageAlt:
-        'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-    },
-    // More products...
-  ]
 </script>
--- src/stores/shoppingCart.js
+++ src/stores/shoppingCart.js
@@ -1,9 +1,39 @@
 import { defineStore } from 'pinia'
+import { useProductsStore } from '@/stores/products';
 
 export const useShoppingCartStore = defineStore('shoppingCart', {
   state: () => ({
-    open: false
+    open: false,
+    products: [
+      {
+        id: 1,
+        name: 'Throwback Hip Bag',
+        href: '#',
+        color: 'Salmon',
+        price: '90',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+        quantity: 1
+      }
+    ]
   }),
   actions: {
+    addProduct(id) {
+      const productsStore = useProductsStore();
+      const product = productsStore.products.find(product => product.id == id)
+
+      if (product) {
+        const existingProduct = this.products.find(p => p.id === product.id);
+
+        if (existingProduct) {
+          existingProduct.quantity += 1;
+        } else {
+          this.products.unshift({
+            ...product,
+            quantity: 1
+          });
+        }
+      }
+    }
   }
 });

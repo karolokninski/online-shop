bff226b29c3fe96715ba339b9d4b3c64114b78c7
jacek <69kotkiwpiwnicy69@gmail.com>
2024-10-28 23:17:21 +0100

    deliverypayment koniec

--- src/components/adminPanel/DeliveryAndPayment.vue
+++ src/components/adminPanel/DeliveryAndPayment.vue
@@ -195,7 +195,7 @@
                     <div class="sm:flex sm:items-start">
                       <div
                         class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <PlusCircleIcon class="h-6 w-6 text-blue-600" aria-hidden="true" />
+                        <PencilSquareIcon class="h-6 w-6 text-blue-600" aria-hidden="true" />
                       </div>
                       <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                         <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Edytuj dostawcę
@@ -376,57 +376,67 @@
     </TransitionRoot>
 
     <TransitionRoot as="addSubpage" :show="addPaymentMethodOpen">
-    <Dialog class="relative z-10" @close="addPaymentMethodOpen = false">
-      <TransitionChild as="addProvider" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
-        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-      </TransitionChild>
+      <Dialog class="relative z-10" @close="addPaymentMethodOpen = false">
+        <TransitionChild as="addProvider" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+          leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
 
-      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <TransitionChild as="addProvider" enter="ease-out duration-300"
-            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
-            leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-              <form @submit.prevent="submitAddPaymentMethodForm">
-                <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div class="sm:flex sm:items-start">
-                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <PlusCircleIcon class="h-6 w-6 text-blue-600" aria-hidden="true" />
-                    </div>
-                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Dodaj formę płatności</DialogTitle>
-                      <div class="mt-2">
-                        <p class="text-sm text-gray-500">Upewnij się, że wszystkie pola są wypełnione.</p>
-                        <div class="border-b border-gray-900/10 pb-8">
-                          <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
-                            <div class="sm:col-span-2">
-                              <label for="add-name" class="block text-sm font-medium leading-6 text-gray-900">Nazwa</label>
-                              <div class="mt-2">
-                                <input v-model="form2.add.method_name" type="text" name="add-name" id="add-name"
-                                  @blur="validateTitle2('add')"
-                                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                <p v-if="form2.add.errors.name" class="text-red-500 text-xs mt-1">{{ form2.add.errors.name }}</p>
+        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <TransitionChild as="addProvider" enter="ease-out duration-300"
+              enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+              leave-from="opacity-100 translate-y-0 sm:scale-100"
+              leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <DialogPanel
+                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <form @submit.prevent="submitAddPaymentMethodForm">
+                  <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div class="sm:flex sm:items-start">
+                      <div
+                        class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <PlusCircleIcon class="h-6 w-6 text-blue-600" aria-hidden="true" />
+                      </div>
+                      <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Dodaj formę
+                          płatności</DialogTitle>
+                        <div class="mt-2">
+                          <p class="text-sm text-gray-500">Upewnij się, że wszystkie pola są wypełnione.</p>
+                          <div class="border-b border-gray-900/10 pb-8">
+                            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
+                              <div class="sm:col-span-2">
+                                <label for="add-name"
+                                  class="block text-sm font-medium leading-6 text-gray-900">Nazwa</label>
+                                <div class="mt-2">
+                                  <input v-model="form2.add.method_name" type="text" name="add-name" id="add-name"
+                                    @blur="validateTitle2('add')"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                  <p v-if="form2.add.errors.name" class="text-red-500 text-xs mt-1">{{
+                                    form2.add.errors.name }}</p>
+                                </div>
                               </div>
-                            </div>
 
-                            <div class="sm:col-span-2">
-                              <label for="add-cost" class="block text-sm font-medium leading-6 text-gray-900">Opłata</label>
-                              <div class="mt-2">
-                                <input v-model="form2.add.fee" type="number" step="0.01" name="add-cost" id="add-cost"
-                                  @blur="validateFee2('add')"
-                                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                <p v-if="form2.add.errors.fee" class="text-red-500 text-xs mt-1">{{ form2.add.errors.fee }}</p>
+                              <div class="sm:col-span-2">
+                                <label for="add-cost"
+                                  class="block text-sm font-medium leading-6 text-gray-900">Opłata</label>
+                                <div class="mt-2">
+                                  <input v-model="form2.add.fee" type="number" step="0.01" name="add-cost" id="add-cost"
+                                    @blur="validateFee2('add')"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                  <p v-if="form2.add.errors.fee" class="text-red-500 text-xs mt-1">{{
+                                    form2.add.errors.fee }}</p>
+                                </div>
                               </div>
-                            </div>
 
-                            <div class="sm:col-span-6">
-                              <label for="add-description" class="block text-sm font-medium leading-6 text-gray-900">Opis</label>
-                              <div class="mt-2">
-                                <textarea v-model="form2.add.description" name="description" id="add-description" rows="3"
-                                  class="block w-full max-h-48 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                              <div class="sm:col-span-6">
+                                <label for="add-description"
+                                  class="block text-sm font-medium leading-6 text-gray-900">Opis</label>
+                                <div class="mt-2">
+                                  <textarea v-model="form2.add.description" name="description" id="add-description"
+                                    rows="3"
+                                    class="block w-full max-h-48 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -434,22 +444,24 @@
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button type="submit" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 sm:ml-3 sm:w-auto">
-                    Dodaj
-                  </button>
-                  <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="addPaymentMethodOpen = false">
-                    Anuluj
-                  </button>
-                </div>
-              </form>
-            </DialogPanel>
-          </TransitionChild>
+                  <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button type="submit"
+                      class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 sm:ml-3 sm:w-auto">
+                      Dodaj
+                    </button>
+                    <button type="button"
+                      class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      @click="addPaymentMethodOpen = false">
+                      Anuluj
+                    </button>
+                  </div>
+                </form>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
+      </Dialog>
+    </TransitionRoot>
 
 
 
@@ -477,7 +489,7 @@
                     <div class="sm:flex sm:items-start">
                       <div
                         class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <PlusCircleIcon class="h-6 w-6 text-blue-600" aria-hidden="true" />
+                        <PencilSquareIcon class="h-6 w-6 text-blue-600" aria-hidden="true" />
                       </div>
                       <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                         <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Edytuj formę
@@ -515,7 +527,7 @@
                                   class="block text-sm font-medium leading-6 text-gray-900">Opis</label>
                                 <div class="mt-2">
                                   <textarea v-model="form2.edit.description" name="edit-description"
-                                    id="edit-description" rows="3" 
+                                    id="edit-description" rows="3"
                                     class="block w-full max-h-48 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                               </textarea>
 
@@ -564,12 +576,6 @@ const providers = ref([]);
 const paymentMethods = ref([]);
 const deletePaymentMethodOpen = ref(false);
 const currentDeletePaymentMethodId = ref(null);
-const newProviderName = ref('');
-const newPaymentMethodName = ref('');
-const editProviderName = ref('');
-const editPaymentMethodName = ref('');
-const providerError = ref('');
-const paymentMethodError = ref('');
 const currentProviderId = ref(null);
 const currentPaymentMethodId = ref(null);
 
@@ -805,7 +811,7 @@ const form2 = ref({
     fee: null,
     errors: {
       name: '',
-      fee:'',
+      fee: '',
     }
   },
 });
@@ -813,7 +819,7 @@ const form2 = ref({
 const validateTitle2 = (formType) => {
 
   const form = formType === 'add' ? form2.value.add : form2.value.edit;
-  
+
   if (!form.method_name) {
     form.errors.name = 'Nazwa nie może być pusta';
   } else {
@@ -822,13 +828,13 @@ const validateTitle2 = (formType) => {
 };
 const validateTitle3 = (formType) => {
 
-const form = formType === 'add' ? form2.value.add : form2.value.edit;
+  const form = formType === 'add' ? form2.value.add : form2.value.edit;
 
-if (!form.name) {
-  form.errors.name = 'Nazwa nie może być pusta';
-} else {
-  form.errors.name = '';
-}
+  if (!form.name) {
+    form.errors.name = 'Nazwa nie może być pusta';
+  } else {
+    form.errors.name = '';
+  }
 };
 
 const validateFee2 = (formType) => {
@@ -837,7 +843,7 @@ const validateFee2 = (formType) => {
 
   const fee = Number(form.fee);
 
-  if (isNaN(fee) || form.fee === '' || form.fee === null || form.fee === undefined|| form.fee === '') {
+  if (isNaN(fee) || form.fee === '' || form.fee === null || form.fee === undefined || form.fee === '') {
     form.errors.fee = 'Opłata nie może być pusta';
   } else if (fee <= 0) {
     form.errors.fee = 'Opłata musi być większa od 0';
@@ -912,9 +918,9 @@ const submitEditPaymentMethodForm = async () => {
     const response = await axios.put(API_URL + `/payment-methods/${currentPaymentMethodId.value}`, updatedPaymentMethodData);
     const index = paymentMethods.value.findIndex(method => method.id === currentPaymentMethodId.value);
     if (index !== -1) {
-      paymentMethods.value[index] = response.data; 
+      paymentMethods.value[index] = response.data;
     }
-    editPaymentMethodOpen.value = false; 
+    editPaymentMethodOpen.value = false;
   } catch (error) {
     console.error("Błąd podczas edytowania metody płatności:", error);
   }

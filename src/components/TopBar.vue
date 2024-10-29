24c8deefaad8350647535be72a0a298758e910b9
jaca123321 <163544801+jaca123321@users.noreply.github.com>
2024-10-29 11:11:05 +0100

    Update TopBar.vue

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -131,7 +131,7 @@
     { name: 'Ulubione', description: "Ulubione produkty", href: '/konto/ulubione', icon: HeartIcon },
   ]
 
-  if (userStore.role === 'Administrator') {
+  if (userStore.role === 'admin') {
     menuItems.push({
       name: 'Admin panel', description: "Panel administracyjny", href: '/admin', icon: UserIcon
     })

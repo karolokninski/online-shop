ee95cb23da41ded0a3384c094503a48bd4b0062e
kepucino <okninskikarol2@gmail.com>
2024-11-16 19:30:12 +0100

    Add product category filter Update product search bar

--- src/components/topBar/SearchBar.vue
+++ src/components/topBar/SearchBar.vue
@@ -7,11 +7,12 @@
           type="text"
           id="product-search" 
           v-model="searchStore.query"
-          class="block w-full h-12 p-4 pe-20 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-50 dark:border-gray-700 dark:placeholder-gray-700 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-           style="width: calc(100% - 1px);"
+          @keyup.enter="handleSearch"
+          class="block w-full h-12 p-4 pe-20 text-md font-semibold text-black border border-gray-400 rounded-xl bg-gray-50 focus:ring-gray-400 focus:border-gray-400"
+          style="width: calc(100% - 1px);"
           placeholder="Czego szukasz?" />
         <XCircleIcon v-show="showCancelButton" @click="searchStore.query = ''" class="text-black absolute end-12 bottom-3 h-6 w-6 mr-1"></XCircleIcon>
-        <button type="submit" @click="handleSearch" class="text-white absolute h-12 w-12 px-3 py-auto end-0 bottom-0 rounded-xl bg-blue-700 hover:bg-blue-800 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700">
+        <button type="submit" @click="handleSearch" class="text-white absolute h-12 w-12 px-3 py-auto end-0 bottom-0 rounded-xl bg-gray-400 hover:bg-gray-500 focus:outline-none active:bg-gray-400">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="black">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>

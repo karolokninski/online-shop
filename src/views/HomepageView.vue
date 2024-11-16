acb9e2dc87a82bf406d019329f7d754da8437eec
kepucino <okninskikarol2@gmail.com>
2024-11-16 21:49:07 +0100

    Update homepage

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -1,24 +1,18 @@
 <template>
   <TopBar></TopBar>
-  <div class="bg-white">
-    <div class="tlo relative isolate px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl py-24">
-        <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Geeked.tech</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-800">W naszym sklepie znajdziesz najwyższej jakości sprzęt komputerowy i podzepoły.</p>
-        </div>
-      </div>
-    </div>
-    <Sale></Sale>
-    <NewsletterAndShops></NewsletterAndShops>
-  </div>
+  <HeroSection></HeroSection>
+  <CategoryPrieview></CategoryPrieview>
+  <Sale></Sale>
+  <NewsletterAndShops></NewsletterAndShops>
   <Footer></Footer>
 </template>
   
 <script setup>
   import TopBar from '@/components/TopBar.vue';
-  import Sale from '@/components/Sales.vue';
-  import NewsletterAndShops from '@/components/NewsletterAndShops.vue';
+  import HeroSection from '@/components/homepage/HeroSection.vue';
+  import CategoryPrieview from '@/components/homepage/CategoryPrieview.vue';
+  import Sale from '@/components/homepage/Sales.vue';
+  import NewsletterAndShops from '@/components/homepage/NewsletterAndShops.vue';
   import Footer from '@/components/Footer.vue';
 </script>
 <style>
